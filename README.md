0ec311b11588728a361f3d58f75f6a99823a83d0
James Blankenship <jeblankenship@gmail.com>
2016-08-28 15:40:58 -0400

    Initial commit version 0.1.2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Move Folder Task
+Build task for VS Team Services that moves folder to destination folder.
+
+
+08/28/2016: This task is not ready for general public use yet.
---
+++ task/move-folder.ps1
@@ -0,0 +1,14 @@
+﻿[CmdletBinding()]
+param(
+    [string] $sourceFolder,
+    [string] $destinationFolder
+)
+
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
+Write-Verbose "Parameter Values"
+$PSBoundParameters.Keys | % { Write-Host "  ${_} = $($PSBoundParameters[$_])" }
+
+Move-Item $sourceFolder $destinationFolder
